b45977f4beda02e97ee93e2e6067a1a76f2d9ff9
jday7879 <57715292+Jday7879@users.noreply.github.com>
2026-01-29 13:46:37 +0000

    initial structure for testing

---
+++ gcc_wrappers/run_from_zip.py
@@ -0,0 +1,16 @@
+from cookiecutter.main import cookiecutter
+import importlib.resources
+
+
+def run_lite_from_zip():
+    resource = importlib.resources.files("gcc_wrappers.zip_templates").joinpath("gcc_manual.zip")
+    with importlib.resources.as_file(resource) as path:
+        cookiecutter(template=str(path))
+
+
+##
+# issue with python created zip file is the folder is not recognised 
+# need to fix it to automate the work but works as a poc 
+
+if __name__ == "__main__":
+    run_lite_from_zip()
